fe444eba2e52d239ee26e9929b89b251088dfbf1
duzipeng <june.dzp@qq.com>
2017-10-26 11:12:54 +0800

    配置任务

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,60 +2,35 @@ module.exports = function (grunt) {
 
   //Project configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      // define the files to lint (定义需要检测的文件)
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      // configure JSHint (documented at http://www.jshint.com/dosc/)
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    concat: {
-      options: {
-        // 定义一个用于插入合并输出文件之间的字符
-        separator: ';'
-      },
-      dist: {
-        // 将要被合并的文件
-        src: ['src/**/*.js'],
-        // 合并后的JS文件的存放位置
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      // 此任务的作用是压缩JavaScript文件
-      options: {
-        // 此处定义的banner注释将插入到输出文件的顶部
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
+    log: {
+      foo: [1, 2, 3],
+      bar: 'hello world',
+      baz: false
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
-  //默认被执行的任务列表
-  grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  // 多任务
+  grunt.registerMultiTask('log', 'Log stuff', function () {
+    grunt.log.writeln(this.target + ': ' + this.data)
+  });
+  // 普通任务
+  grunt.registerTask('foo', 'my "foo" task.', function () {
+    grunt.task.run('bar', 'baz');
+  });
+  // 默认任务
+  grunt.registerTask('default', 'My "default" task description.', function () {
+    grunt.log.writeln('Currently running the "default" task.');
+  });
+  // 自定义任务
+  grunt.registerTask('asyncfoo', 'My "asyncfoo" task.', function () {
+    // Force task into async mode and grab a handle to the "done" function.
+    var done = this.async();
+    // Run some sync stuff.
+    grunt.log.writeln('Processing task...');
+    // And some async stuff.
+    setTimeout(function () {
+      grunt.log.writeln('All done!');
+      done();
+    }, 1000)
+  })
 }
